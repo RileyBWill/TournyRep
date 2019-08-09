0908df6ea6ca3b34362b8f031607760f497471a2
Murph Murphy <omurcada@gmail.com>
2019-08-09 04:19:09 -0600

    Add react/redux and initial component.

---
+++ tourny-rep/src/components/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "./style.css";
+
+function App() {
+  return <div>Hello from Bozeman!</div>;
+}
+
+export default App;
--- tourny-rep/webpack.main.config.js
+++ tourny-rep/webpack.main.config.js
@@ -3,7 +3,7 @@ module.exports = {
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: "./src/main.ts",
+  entry: "./src/main/main.ts",
   devtool: "inline-source-map",
   // Put your normal webpack config below here
   module: {

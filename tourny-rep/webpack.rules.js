1982e9b9daccc029d8138e9be8d97407e566a587
Murph Murphy <omurcada@gmail.com>
2019-08-09 03:45:50 -0600

    Initial commit. Modified electron-forge --webpack for TS

---
+++ tourny-rep/webpack.rules.js
@@ -0,0 +1,39 @@
+module.exports = [
+  // Add support for native node modules
+  {
+    test: /\.node$/,
+    use: "node-loader"
+  },
+  {
+    test: /\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: "@marshallofsound/webpack-asset-relocator-loader",
+      options: {
+        outputAssetBase: "native_modules"
+      }
+    }
+  },
+  {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/
+  }
+
+  // Put your webpack loader rules in this array.  This is where you would put
+  // your ts-loader configuration for instance:
+  /**
+   * Typescript Example:
+   *
+   * {
+   *   test: /\.tsx?$/,
+   *   exclude: /(node_modules|.webpack)/,
+   *   loaders: [{
+   *     loader: 'ts-loader',
+   *     options: {
+   *       transpileOnly: true
+   *     }
+   *   }]
+   * }
+   */
+];
